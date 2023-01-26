d505f785af2a08f008566c30dd900803f476ad8c
lucas.hsu <lucas.hsu@coretronic.com>
2023-01-26 12:18:19 +0800

    Authorization system in Django 1. Login / Logout page working 2. require a login to reach a certain route

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
 def home(request):
     context = {
         'posts': Post.objects.all()
